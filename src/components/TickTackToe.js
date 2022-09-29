309b462b4f51265c906bcd968bf80f0423466397
unknown <epeng1994@gmail.com>
2022-09-29 19:13:42 -0400

    added function to check if player wins

--- src/components/TickTackToe.js
+++ src/components/TickTackToe.js
@@ -5,17 +5,32 @@ const PLAYER_ONE = 'PLAYER 1'
 const PLAYER_TWO = 'PLAYER 2'
 
 function TickTackToe(){
-    const [grid, setGrid]= useState(['1','2','3','4','5','6','7','8','9'])
+    const [grid, setGrid]= useState(['','','','','','','','',''])
     const [player,setPlayer] = useState(PLAYER_ONE)
+    const [winner,setWinner] = useState('')
 
     const onGridChange = e =>{
-        //console.log(e.target.attributes.idx.value)
         const currentPlayer =  player === 'PLAYER 1' ? 'X' : 'O'
         let cloneGrid = [...grid]
-        cloneGrid[e.target.attributes.idx.value] = currentPlayer
-        setPlayer(currentPlayer === 'X' ? PLAYER_TWO:PLAYER_ONE)
-        setGrid(cloneGrid)
-        //console.log(currentPlayer)
+        if(cloneGrid[e.target.attributes.idx.value] === ''){
+            cloneGrid[e.target.attributes.idx.value] = currentPlayer
+            setPlayer(currentPlayer === 'X' ? PLAYER_TWO:PLAYER_ONE)
+            setGrid(cloneGrid)
+        }
+        //win conditions 0-1-2 3-4-5 6-7-8 0-5-8 1-5-7 2-5-6
+        if((cloneGrid[0]===cloneGrid[1]&&cloneGrid[1]===cloneGrid[2]&&cloneGrid[0]!==''&&cloneGrid[1]!==''&&cloneGrid[2]!=='')||
+        (cloneGrid[3]===cloneGrid[4]&&cloneGrid[4]===cloneGrid[5]&&cloneGrid[3]!==''&&cloneGrid[4]!==''&&cloneGrid[5]!=='')||
+        (cloneGrid[6]===cloneGrid[7]&&cloneGrid[7]===cloneGrid[8]&&cloneGrid[6]!==''&&cloneGrid[7]!==''&&cloneGrid[8]!=='')||
+        (cloneGrid[0]===cloneGrid[3]&&cloneGrid[3]===cloneGrid[6]&&cloneGrid[0]!==''&&cloneGrid[3]!==''&&cloneGrid[6]!=='')||
+        (cloneGrid[1]===cloneGrid[4]&&cloneGrid[4]===cloneGrid[7]&&cloneGrid[1]!==''&&cloneGrid[4]!==''&&cloneGrid[7]!=='')||
+        (cloneGrid[2]===cloneGrid[5]&&cloneGrid[5]===cloneGrid[8]&&cloneGrid[2]!==''&&cloneGrid[5]!==''&&cloneGrid[8]!=='')){
+            setWinner(player)
+        }
+    }
+    const resetGrid=e=>{
+        setGrid(['','','','','','','','',''])
+        setPlayer(PLAYER_ONE)
+        setWinner('')
     }
 
     useEffect(()=>{
@@ -26,6 +41,7 @@ function TickTackToe(){
         <>
         <div>
             <div>Current Player: {player}</div>
+            {winner ? <div>{winner}</div> : null}
             <div className = 'container'>
             {
                 grid.map((a,i)=>{
@@ -35,9 +51,9 @@ function TickTackToe(){
                 })
             }
             </div>
+            <button onClick = {()=>resetGrid()}>Reset</button>
 
         </div>
-
         </>
     )
 };
