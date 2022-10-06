635ea26e077e45f76fd84015758b0cac0e0a6ad3
unknown <epeng1994@gmail.com>
2022-10-06 01:26:01 -0400

    implemented background music

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import TickTackToe from './components/TickTackToeGame/TickTackToe';
 import Banner from './components/Banner/index';
+import BackgroundMusic from './background.mp3'
 
 function App() {
+  const audio = new Audio(BackgroundMusic)
+  audio.loop = true
+  audio.play()
+
+
   return (
     <div className="App">
+
       <header className="App-header">
       </header>
       <Banner/>
